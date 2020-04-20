3fbc66e9421a5c8340a2ed570192401ec0f289f4
kelbikhanov <rusik9091@gmail.com>
2020-04-20 23:10:51 +0300

    formLogin

---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import {Block, Button} from "../../components";
+import { Form, Input, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './Auth.scss';
+
+const Auth = () => {
+    const onFinish = values => {
+        console.log('Received values of form: ', values);
+    };
+    return (
+        <section className="auth">
+            <div className="auth__content">
+                <div className="auth__top">
+                    <h2>Войти в аккаунт</h2>
+                    <p>Пожалуйста, войдите в свой аккаунт</p>
+                </div>
+                <Block>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={onFinish}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Username!',
+                                },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Password!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <a className="login-form-forgot" href="">
+                                Forgot password
+                            </a>
+                        </Form.Item>
+
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                            Or <a href="">register now!</a>
+                        </Form.Item>
+                    </Form>
+                </Block>
+            </div>
+        </section>
+
+    );
+};
+export default Auth;
