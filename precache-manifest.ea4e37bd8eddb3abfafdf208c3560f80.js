7f886151ba4924458bff52e8794c3818379d4dcb
kelbikhanov <rusik9091@gmail.com>
2020-06-13 12:49:46 +0300

    Updates

---
+++ precache-manifest.ea4e37bd8eddb3abfafdf208c3560f80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1f419d6abb1c32f97508b61040b2074",
+    "url": "/ChatDesctop/static/media/readed.c1f419d6.svg"
+  },
+  {
+    "revision": "3c841bf605a8a476802d71028127e33b",
+    "url": "/ChatDesctop/static/media/noreaded.3c841bf6.svg"
+  },
+  {
+    "revision": "ad5a5a11dd4de66a7bb1",
+    "url": "/ChatDesctop/static/js/runtime~main.f70cd8ce.js"
+  },
+  {
+    "revision": "4f4da7f2bf2e32aa1ca6",
+    "url": "/ChatDesctop/static/js/main.c60a36cf.chunk.js"
+  },
+  {
+    "revision": "92720cdfd1fe72d23484",
+    "url": "/ChatDesctop/static/js/2.0773be65.chunk.js"
+  },
+  {
+    "revision": "4f4da7f2bf2e32aa1ca6",
+    "url": "/ChatDesctop/static/css/main.acd05a1f.chunk.css"
+  },
+  {
+    "revision": "92720cdfd1fe72d23484",
+    "url": "/ChatDesctop/static/css/2.8c65ae87.chunk.css"
+  },
+  {
+    "revision": "f5e70a94eed4290084936f6553b9834c",
+    "url": "/ChatDesctop/index.html"
+  }
+];
